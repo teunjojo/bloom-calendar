ae78abd836fff25270ea1bc116dc1725902b63f5
Teuntje Kuipers <mail@teunjojo.com>
2025-12-06 16:04:54 +0100

    Enable CORS in the bootstrap function

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
