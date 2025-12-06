fe39bbc10c5cd272ed9c4215183c256502e77b11
Teuntje Kuipers <mail@teunjojo.com>
2025-12-06 16:13:45 +0100

    Add Events route and create EventsView component

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,7 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{
+    path: '/events',
+    name: 'Events',
+    component: () => import('@/views/EventsView.vue'),
+  }],
 })
 
 export default router
