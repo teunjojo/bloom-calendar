0182d30cc72f3a88ca2f363f881b00f33ebd8222
Teuntje Kuipers <mail@teunjojo.com>
2025-12-06 20:40:02 +0100

    Add big flower forecast display with images to event view

--- frontend/src/views/EventsView.vue
+++ frontend/src/views/EventsView.vue
@@ -35,11 +35,24 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex flex-col event-list gap-2 w-96">
+  <div class="event-list flex flex-col gap-2 w-96">
     <span class="text-xl font-bold">Current Events</span>
     <div v-for="event in events" :key="event.id" class="event-container">
       <h2 class="text-xl font-bold">{{ event.name }}</h2>
       <div class="text-sm">Until {{ formatDate(new Date(event.endDate)) }}</div>
+      <div v-if="event.bigFlowers.length > 0" class="big-flower-forecast">
+        <span class="text-md font-bold">Big Flower Forecast</span>
+        <div class="flower-list flex justify-center flex-wrap gap-2 mt-2">
+          <div
+            v-for="flower in event.bigFlowers"
+            :key="flower.id"
+            class="flower-container flex flex-col items-center"
+          >
+            <img class="w-20" :src="`/images/flowers/${flower.slug}.png`" />
+            <span class="text-md">{{ flower.name }}</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -54,5 +67,13 @@ onMounted(() => {
   border-radius: 1rem;
   background-color: var(--primary-color);
   color: white;
+}
+
+.big-flower-forecast {
+  margin-top: 1rem;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  background-color: white;
+  color: var(--primary-color);
 }
 </style>
