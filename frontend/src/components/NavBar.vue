0291de2ce8edb6e87754751b883603034da098d6
Teuntje Kuipers <mail@teunjojo.com>
2025-12-07 22:17:22 +0100

    Refactor NavBar component layout and update title to BloomCalendar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav class="flex p-2 w-100 justify-center items-center bg-white shadow-sm gap-4">
-    <img src="/images/icons/logo.png" style="height: 3rem" />
-    <h1 class="text-2xl font-bold">Pikmin Bloom Overview</h1>
+  <nav class="flex p-2 justify-center items-center bg-white px-4">
+    <ul class="flex justify-between items-center gap-4" style="width: 100%; max-width: 720px">
+      <li>
+        <RouterLink to="/" class="flex items-center gap-2">
+          <img src="/images/icons/logo.png" style="height: 3rem" />
+          <h1 class="text-2xl font-bold">BloomCalendar</h1>
+        </RouterLink>
+      </li>
+      <li><RouterLink to="/login">Login</RouterLink></li>
+    </ul>
   </nav>
 </template>
 
