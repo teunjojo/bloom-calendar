d74b86d5c171f71b2727254e7637e2084f0b07a2
Teuntje Kuipers <mail@teunjojo.com>
2025-12-07 22:17:07 +0100

    Rename DashboardView component to HomeView

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,9 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [{
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: () => import('@/views/DashboardView.vue'),
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/HomeView.vue'),
   }],
 })
 
