ab9994345d4a47fb5c048fc68773596d40f76c9f
Teuntje Kuipers <mail@teunjojo.com>
2025-12-07 04:04:38 +0100

    Add NavBar component and integrate it into App.vue

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="flex p-4 w-100 justify-center items-center border">
+    <h1 class="text-2xl font-bold">Pikmin Bloom Overview</h1>
+  </nav>
+</template>
+
+<style scoped></style>
