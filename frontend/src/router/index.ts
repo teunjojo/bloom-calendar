488461ecf017af3ff72268a35a59adbc698a4287
Teuntje Kuipers <mail@teunjojo.com>
2025-12-07 03:22:59 +0100

    Rename EventsView to DashboardView

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,9 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [{
-    path: '/events',
-    name: 'Events',
-    component: () => import('@/views/EventsView.vue'),
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: () => import('@/views/DashboardView.vue'),
   }],
 })
 
