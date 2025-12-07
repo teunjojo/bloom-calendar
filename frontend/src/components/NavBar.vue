1fe5a08e7446df9e178caeab756335b84e7b694b
Teuntje Kuipers <mail@teunjojo.com>
2025-12-07 04:54:27 +0100

    Add logo image to navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav class="flex p-4 w-100 justify-center items-center border">
+  <nav class="flex p-2 w-100 justify-center items-center border">
+    <img src="/images/icons/logo.png" style="height: 3rem" />
     <h1 class="text-2xl font-bold">Pikmin Bloom Overview</h1>
   </nav>
 </template>
