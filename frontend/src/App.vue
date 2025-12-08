656f98c7341d1c69e6b84451218ec727d193f9f7
Teuntje Kuipers <mail@teunjojo.com>
2025-12-08 10:09:01 +0100

    Update index.html and App.vue to include dynamic title handling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 import NavBar from '@/components/NavBar.vue'
 import FooterComponent from '@/components/FooterComponent.vue'
-import { onMounted } from 'vue'
-import { RouterView } from 'vue-router'
+import { onMounted, watch } from 'vue'
+import { RouterView, useRoute } from 'vue-router'
 
 import { checkAuth } from '@/service/authService'
 
+const route = useRoute()
+watch(
+  () => route.name?.toString(),
+  (newName) => {
+    if (!newName) {
+      document.title = 'BloomCalendar'
+      return
+    }
+    document.title = `BloomCalendar - ${newName}`
+  },
+  { immediate: true }
+)
 
 onMounted(async () => {
   checkAuth().catch(() => {
