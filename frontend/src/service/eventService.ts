ee16285001b9dde558f1b949871627d48d019de7
Teuntje Kuipers <mail@teunjojo.com>
2025-12-12 21:49:20 +0100

    Fix API endpoint in getEvents function to use plural '/events'

--- frontend/src/service/eventService.ts
+++ frontend/src/service/eventService.ts
@@ -3,7 +3,7 @@ import type { EventFilter } from '@/types/EventFilter'
 import api from '@/axiosClient'
 
 export const getEvents = async (filters: EventFilter = {}): Promise<PikminEvent[]> => {
-  const response = await api.get<PikminEvent[]>('/event', {
+  const response = await api.get<PikminEvent[]>('/events', {
     params: filters,
   })
   return response.data
