8a41cb5570ca92262fc8f0053229e1327fff53cc
Teuntje Kuipers <mail@teunjojo.com>
2025-12-17 01:50:16 +0100

    Remove old backend code

--- backend-old/src/config/typeorm.config.ts
+++
@@ -1,8 +0,0 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-
-export const typeOrmConfig = (): TypeOrmModuleOptions => ({
-  type: 'sqlite',
-  database: 'database.sqlite',
-  autoLoadEntities: true,
-  synchronize: true,
-});
--- backend-old/src/forecast/dto/forecast-filter.dto.ts
+++
@@ -1,3 +0,0 @@
-import { EventFilterDto } from '@app/event/dto/event-filter.dto';
-
-export class ForecastFilterDto extends EventFilterDto {}
