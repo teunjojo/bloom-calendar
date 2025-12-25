4f88ee54db7f2101ee75741af3b882206550403f
Teuntje Kuipers <mail@teunjojo.com>
2025-12-25 03:44:40 +0100

    Add auth route with login, refresh, logout and check endpoints

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,69 @@
+import { Context, Hono } from 'hono';
+import prismaClients from '@/db/prisma';
+import { z } from 'zod';
+
+import { jwt } from 'hono/jwt';
+import type { JwtVariables } from 'hono/jwt';
+import { HTTPException } from 'hono/http-exception';
+import { newToken, signIn } from '@/services/authService';
+import { deleteCookie, getCookie, setCookie } from 'hono/cookie';
+
+type Variables = JwtVariables;
+
+const authRouter = new Hono<{ Variables: Variables }>();
+
+authRouter.post('login', async (c: Context) => {
+	const json = await c.req.json();
+
+	const username = z.string().min(1).parse(json.username);
+	const password = z.string().min(1).parse(json.password);
+
+	const db = c.env.bloom_calendar_database;
+	const prisma = await prismaClients.fetch(db);
+
+	const { accessToken, refreshToken } = await signIn(prisma, username, password, c.env.JWT_SECRET);
+
+	const cookie = setCookie(c, 'refresh_token', refreshToken, {
+		httpOnly: true,
+		secure: false,
+		sameSite: 'strict',
+		maxAge: 7 * 24 * 60 * 60, // 7 days
+	});
+
+	return c.json({ accessToken });
+});
+
+authRouter.post('refresh', async (c: Context) => {
+	const cookie = getCookie(c, 'refresh_token');
+
+	if (!cookie) return c.json({ error: 'Cookie not set' }, 400);
+
+	try {
+		const { accessToken, refreshToken } = await newToken(cookie!, c.env.JWT_SECRET);
+
+		return c.json({ accessToken, refreshToken });
+	} catch (e) {
+		return c.json({ error: 'Failed to refresh token' }, 401);
+	}
+});
+
+authRouter.post('logout', (c: Context) => {
+	deleteCookie(c, 'refresh_token');
+
+	return c.json({success: true})
+});
+
+authRouter.use('*', (c, next) => {
+	const jwtMiddleware = jwt({
+		secret: (c.env as {JWT_SECRET: string}).JWT_SECRET,
+	});
+	return jwtMiddleware(c, next);
+});
+//
+// Everything below this line requires the client to be authenticated
+//
+
+authRouter.post('check', async (c: Context) => {
+	return c.json({ success: true})
+});
+export default authRouter;
---
+++ backend/src/services/authService.ts
@@ -0,0 +1,45 @@
+import { HTTPException } from 'hono/http-exception';
+
+import { getUser } from './userService';
+import { sign, verify } from 'hono/jwt';
+import { PrismaClient } from '@/generated/prisma';
+
+export async function signIn(
+	prisma: PrismaClient,
+	username: string,
+	pass: string,
+	jwtSecret: string
+): Promise<{ accessToken: string; refreshToken: string }> {
+	const users = await getUser(prisma, username);
+	const user = users[0];
+
+	console.log('%s == %s', user.password, pass);
+	// TODO: Use hashed passwords
+	if (user?.password !== pass) {
+		throw new HTTPException(401);
+	}
+	const accessPayload = { sub: user.id, username: user.username, exp: Math.floor(Date.now() / 1000) + 15 * 60 }; // 15 minutes
+	const refreshPayload = { sub: user.id, username: user.username, exp: Math.floor(Date.now() / 1000) + 7 * 24 * 60 * 60 };
+
+	const accessToken = await sign(accessPayload, jwtSecret);
+	const refreshToken = await sign(refreshPayload, jwtSecret);
+
+	return { accessToken: accessToken, refreshToken: refreshToken };
+}
+
+export async function newToken(refreshToken: string, jwtSecret: string): Promise<{ accessToken: string; refreshToken: string }> {
+	if (refreshToken.length == 0) throw new HTTPException(401);
+
+	const payload = await verify(refreshToken, jwtSecret);
+
+	const accessPayload = payload;
+	const refreshPayload = payload;
+
+	accessPayload.exp = Date.now() / 1000 + 60 * 15; // 15m
+	refreshPayload.exp = Date.now() / 1000 + 60 * 60 * 24 * 7; // 7d
+
+	const accessToken = await sign(accessPayload, jwtSecret);
+	const newRefreshToken = await sign(refreshPayload, jwtSecret);
+
+	return { accessToken, refreshToken: newRefreshToken };
+}
