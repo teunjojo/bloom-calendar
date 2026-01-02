1cf57a53f32f52c1ffd9eac2282acff41d52de5a
Teuntje Kuipers <mail@teunjojo.com>
2026-01-02 23:27:22 +0100

    Add  POST `/events/:id/public' endpoint

--- backend/src/routes/event.ts
+++ backend/src/routes/event.ts
@@ -1,8 +1,9 @@
 import { Context, Hono } from 'hono';
 import prismaClients from '@/db/prisma';
 import { z } from 'zod';
-import { getEvents } from '@/services/eventHandler';
+import { getEvents, updateEventPublicState } from '@/services/eventHandler';
 import { eventFilterSchema } from '@/schemas/event-filter';
+import { jwt } from 'hono/jwt';
 
 const eventRouter = new Hono();
 
@@ -26,5 +27,32 @@ eventRouter.get('/', async (c: Context) => {
 
 	const events = await getEvents(prisma, query);
 	return c.json(events);
+});
+
+eventRouter.use('*', (c, next) => {
+	const jwtMiddleware = jwt({
+		secret: (c.env as { JWT_SECRET: string }).JWT_SECRET,
+	});
+	return jwtMiddleware(c, next);
+});
+//
+// Everything below this line requires the client to be authenticated
+//
+
+eventRouter.post('/:id/public', async (c: Context) => {
+	const id = parseInt(c.req.param('id'));
+	const body = await c.req.json();
+	const state = body.state;
+
+	const db = c.env.bloom_calendar_database;
+	const prisma = await prismaClients.fetch(db);
+
+	if (!prisma) {
+		return c.json({ error: 'Database not found' }, 500);
+	}
+
+	const updatedEvent = await updateEventPublicState(prisma, id, state);
+
+	return c.json(updatedEvent);
 });
 export default eventRouter;
