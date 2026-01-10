9d556a62d77f0023144882f01be060f2b5c8e28f
Teuntje Kuipers <mail@teunjojo.com>
2026-01-10 02:46:11 +0100

    Add newDecor and returningDecor arrays to event-input

--- api/src/schemas/even-input.ts
+++ api/src/schemas/even-input.ts
@@ -1,17 +1,32 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
 export const EventInputSchema = z.object({
-  name: z.string().min(1),
-  blogLink: z.url().optional().nullable(),
-  startDate: z.string().refine((val) => !isNaN(Date.parse(val))),
-  endDate: z.string().refine((val) => !isNaN(Date.parse(val))),
-  public: z.boolean(),
-  images: z.array(
-    z.object({
+	id: z.number().optional(),
+	name: z.string().min(1),
+	blogLink: z.url().optional().nullable(),
+	startDate: z.string().refine((val) => !isNaN(Date.parse(val))),
+	endDate: z.string().refine((val) => !isNaN(Date.parse(val))),
+	public: z.boolean(),
+	images: z.array(
+		z.object({
 			id: z.number().optional(),
-      imageUrl: z.url(),
-    })
-  ),
-})
+			imageUrl: z.url(),
+		}),
+	),
+	newDecor: z.array(
+		z.object({
+			id: z.number().optional(),
+			name: z.string(),
+			type: z.string(),
+		}),
+	),
+	returningDecor: z.array(
+		z.object({
+			id: z.number().optional(),
+			name: z.string(),
+			type: z.string(),
+		}),
+	),
+});
 
-export type EventInput = z.infer<typeof EventInputSchema>
+export type EventInput = z.infer<typeof EventInputSchema>;
