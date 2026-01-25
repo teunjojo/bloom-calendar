b3c4cea9d1c6f687e7748cedc60bcff0ad0854e9
Teuntje Kuipers <mail@teunjojo.com>
2026-01-25 18:06:18 +0100

    Update footer to begin begin year in copyright notice

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,9 +2,9 @@
 
 <template>
   <footer class="text-center p-4 text-sm bg-gradient-to-b from-green-200 to-white text-gray-700">
-    &copy; {{ new Date().getFullYear() }} BloomCalendar • by
-    <a class="underline font-bold" href="https://www.teunjojo.com/">teunjojo</a><br />Not affiliated with
-    Nintendo or Niantic.
+    &copy; 2025-{{ new Date().getFullYear() }} BloomCalendar • by
+    <a class="underline font-bold" href="https://www.teunjojo.com/">teunjojo</a><br />Not affiliated
+    with Nintendo or Niantic.
   </footer>
 </template>
 
