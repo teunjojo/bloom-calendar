a73f88ea8544a08ab29082b78a6fa18e5558d119
Teuntje Kuipers <mail@teunjojo.com>
2026-05-17 19:50:06 +0200

    Add latest commit hash to page

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { execSync } from 'node:child_process'
+
+const commitHash = execSync('git rev-parse --short HEAD')
+  .toString()
+  .trim()
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +15,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  define: {
+    __COMMIT_HASH__: JSON.stringify(commitHash),
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
